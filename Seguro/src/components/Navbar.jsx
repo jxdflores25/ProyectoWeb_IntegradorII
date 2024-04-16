68182ab63df386d1e405ecc2bb2ac543832e3c1b
Johann <johann.polo07@gmail.com>
2024-04-16 01:18:57 -0500

    :white_check_mark: login end (part front)

--- Seguro/src/components/Navbar.jsx
+++ Seguro/src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-              <a href="#" className="py-2 px-3 border-4 border-black rounded-md"> Iniciar sesión</a>
+              <a href="/login" className="py-2 px-3 border-4 border-black rounded-md"> Iniciar sesión</a>
               <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800 text-white" >Crear cuenta</a>
             </div>
           </div>
