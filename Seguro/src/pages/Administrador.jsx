755cf998a14f3a8203193dfc98d42a7324ceb547
jxdflores25 <jxdflores25@gmail.com>
2024-04-18 11:26:50 -0500

    Manejando las sesiones para los diferentes usuarios

--- Seguro/src/pages/Administrador.jsx
+++ Seguro/src/pages/Administrador.jsx
@@ -1,29 +1,52 @@
+import { Navigate } from "react-router-dom";
+import { GetAdministrador } from "../API/API_Seguro";
+import { useEffect, useState } from "react";
+
 const Administrador = () => {
-	return (
-        <div className="flex flex-col items-center mt-6 lg:mt-20">
-        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-    Bienvenido&nbsp;
-    <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-      Administrador
-    </span>
-  </h1>
-  
-        <p className="mt-10 tex-lg text-center tex-neutral-500 max-w-4xl">
-          En MediSalud, nuestra pasión es impulsar un estilo de vida saludable y
-          feliz a través de productos y servicios que inspiran, nutren y
-          revitalizan cuerpo y mente. Descubre cómo nuestra innovación y
-          compromiso transforman tu bienestar en una experiencia extraordinaria y
-          sostenible.
-        </p>
-        <div className="flex mt-10 justify-center">
-          <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-500 mx-2 my-4">
-              <source src={medicina1} type="video/mp4"/>
-          </video>
-          <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-500 mx-2 my-4">
-              <source src={medicina2} type="video/mp4"/>
-          </video>
-        </div>
-      </div>
-	);
-}
-export default Administrador
+  const [Administrador, setAdministrador] = useState({
+    nombre: "Usuario",
+    apellido: "",
+  });
+
+  useEffect(() => {
+    const Datos = async (dni) => {
+      const resp = await GetAdministrador(dni);
+      setAdministrador(resp.data);
+    };
+    Datos(localStorage.getItem("usuario"));
+  }, []);
+
+  if (localStorage.getItem("tipo") !== "Administrador") {
+    return <Navigate to="/" />;
+  }
+
+  const deleteStorage = () => {
+    localStorage.removeItem("usuario");
+    localStorage.removeItem("tipo");
+    window.location.href = "/";
+  };
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        Bienvenido Administrador: &nbsp;
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          {Administrador.nombre + " " + Administrador.apellido}
+        </span>
+      </h1>
+
+      <p className="mt-10 tex-lg text-center tex-neutral-500 max-w-4xl">
+        En MediSalud, nuestra pasión es impulsar un estilo de vida saludable y
+        feliz a través de productos y servicios que inspiran, nutren y
+        revitalizan cuerpo y mente. Descubre cómo nuestra innovación y
+        compromiso transforman tu bienestar en una experiencia extraordinaria y
+        sostenible.
+      </p>
+      <button
+        onClick={deleteStorage}
+        className=" active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-out transition-all py-3 rounded-xl bg-orange-500 text-white text-lg fond bold">
+        Cerrar Sesion
+      </button>
+    </div>
+  );
+};
+export default Administrador;
--- Seguro/src/pages/Repartidor.jsx
+++ Seguro/src/pages/Repartidor.jsx
@@ -1,29 +1,52 @@
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { GetConductor } from "../API/API_Seguro";
+
 const Repartidor = () => {
-    return (
-        <div className="flex flex-col items-center mt-6 lg:mt-20">
-        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-    Bienvenido&nbsp;
-    <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-      Repartidor
-    </span>
-  </h1>
-  
-        <p className="mt-10 tex-lg text-center tex-neutral-500 max-w-4xl">
-          En MediSalud, nuestra pasión es impulsar un estilo de vida saludable y
-          feliz a través de productos y servicios que inspiran, nutren y
-          revitalizan cuerpo y mente. Descubre cómo nuestra innovación y
-          compromiso transforman tu bienestar en una experiencia extraordinaria y
-          sostenible.
-        </p>
-        <div className="flex mt-10 justify-center">
-          <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-500 mx-2 my-4">
-              <source src={medicina1} type="video/mp4"/>
-          </video>
-          <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-500 mx-2 my-4">
-              <source src={medicina2} type="video/mp4"/>
-          </video>
-        </div>
-      </div>
-    )
-}
-export default Repartidor
+  const [Repartidor, setRepartidor] = useState({
+    nombre: "Usuario",
+    apellido: "",
+  });
+
+  useEffect(() => {
+    const Datos = async (dni) => {
+      const resp = await GetConductor(dni);
+      setRepartidor(resp.data);
+    };
+    Datos(localStorage.getItem("usuario"));
+  }, []);
+
+  if (localStorage.getItem("tipo") !== "Conductor") {
+    return <Navigate to="/" />;
+  }
+
+  const deleteStorage = () => {
+    localStorage.removeItem("usuario");
+    localStorage.removeItem("tipo");
+    window.location.href = "/";
+  };
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        Bienvenido Repartidor: &nbsp;
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          {Repartidor.nombre + " " + Repartidor.apellido}
+        </span>
+      </h1>
+
+      <p className="mt-10 tex-lg text-center tex-neutral-500 max-w-4xl">
+        En MediSalud, nuestra pasión es impulsar un estilo de vida saludable y
+        feliz a través de productos y servicios que inspiran, nutren y
+        revitalizan cuerpo y mente. Descubre cómo nuestra innovación y
+        compromiso transforman tu bienestar en una experiencia extraordinaria y
+        sostenible.
+      </p>
+      <button
+        onClick={deleteStorage}
+        className=" active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-out transition-all py-3 rounded-xl bg-orange-500 text-white text-lg fond bold">
+        Cerrar Sesion
+      </button>
+    </div>
+  );
+};
+export default Repartidor;
