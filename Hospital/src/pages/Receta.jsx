3c84d42cc3591e424b3b310a704decb3bfe79139
jxdflores25 <jxdflores25@gmail.com>
2024-04-18 01:43:24 -0500

    Arreglando bug

--- Hospital/src/pages/Receta.jsx
+++ Hospital/src/pages/Receta.jsx
@@ -77,6 +77,7 @@ export default function Receta() {
       selectMedicamento.removeChild(optionToRemove);
     }
   };
+
   const medicamento = {};
   const handleAdd = () => {
     const value = document.querySelector("#medicamento").value;
