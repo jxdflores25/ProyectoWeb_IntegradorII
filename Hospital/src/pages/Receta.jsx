0741b8b1af0c4915f5c809a9ad04819b3c6b72b1
jxdflores25 <jxdflores25@gmail.com>
2024-04-18 00:20:15 -0500

    Mejorando cosas

--- Hospital/src/pages/Receta.jsx
+++ Hospital/src/pages/Receta.jsx
@@ -63,112 +63,142 @@ export default function Receta() {
 
   return (
     <div className="flex justify-center items-center h-screen bg-slate-600 ">
-    <section className="flex flex-col gap-4 bg-stone-400 justify-center items-center p-4">
-      <h2 className="w-full text-center font-bold text-5xl my-5 ">RECETA</h2>
-      <div className="flex flex-col gap-4 w-full px-20">
-        <header className="flex justify-between">
-          <p>
-            FECHA<input className="w-9/12 border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent" type="text" value={day} disabled />
-          </p>
-          <p>
-            DNI <input className="w-9/12 border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent" type="text" id="DNI" />
-          </p>
-          <p>
-            MEDICO{" "}
-            <input className="w-9/12 border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent" type="text" value="Juan Carlos" disabled id="NombreMedico" />
-          </p>
-        </header>
-        <section className="">
-          <div className="flex justify-center items-center">
-          <form onSubmit={handleSubmit} className=" flex flex-col w-full ">
-            <div className="flex justify-evenly gap-5">
-            <label htmlFor="" className="">Medicamento:</label>
-            <select name="medicamento" id="medicamento" className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent" >
-              <option value="" selected disabled hidden c>
-                Elige Medicamento
-              </option>
-              
-              <option className="" value="Aspirina">Aspirina</option>
-              <option value="Frutenzima">Frutenzima</option>
-              <option value="Ceterizina">Ceterizina</option>
-              <option value="Cetanicol">Cetanicol</option>
-            </select>
-            
-            <label htmlFor="">Descripcion:</label>{" "}
-            <input type="text" id="descripcion" name="descripcion" className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent" />
+      <section className="flex flex-col gap-4 bg-stone-400 justify-center items-center p-4">
+        <h2 className="w-full text-center font-bold text-5xl my-5 ">RECETA</h2>
+        <div className="flex flex-col gap-4 w-full px-20">
+          <header className="flex justify-between">
+            <p>
+              FECHA
+              <input
+                className="w-9/12 border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                type="text"
+                value={day}
+                disabled
+              />
+            </p>
+            <p>
+              DNI{" "}
+              <input
+                className="w-9/12 border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                type="text"
+                id="DNI"
+              />
+            </p>
+            <p>
+              MEDICO{" "}
+              <input
+                className="w-9/12 border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                type="text"
+                value="Juan Carlos"
+                disabled
+                id="NombreMedico"
+              />
+            </p>
+          </header>
+          <section className="">
+            <div className="flex justify-center items-center">
+              <form onSubmit={handleSubmit} className=" flex flex-col w-full ">
+                <div className="flex justify-evenly gap-5">
+                  <label htmlFor="" className="">
+                    Medicamento:
+                  </label>
+                  <select
+                    name="medicamento"
+                    id="medicamento"
+                    className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent">
+                    <option value="" selected disabled hidden c>
+                      Elige Medicamento
+                    </option>
+
+                    <option className="" value="Aspirina">
+                      Aspirina
+                    </option>
+                    <option value="Frutenzima">Frutenzima</option>
+                    <option value="Ceterizina">Ceterizina</option>
+                    <option value="Cetanicol">Cetanicol</option>
+                  </select>
+                  <label htmlFor="">Descripcion:</label>{" "}
+                  <input
+                    type="text"
+                    id="descripcion"
+                    name="descripcion"
+                    className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                  />
+                </div>
+                <div className="flex justify-evenly gap-5 mt-5">
+                  <label htmlFor="">Cantidad:</label>{" "}
+                  <input type="number" id="cantidad" name="cantidad" />
+                  <input
+                    type="submit"
+                    value="Agregar"
+                    onClick={handleAdd}
+                    className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-out transition-all py-1 rounded-xl bg-orange-500 text-white text-lg fond bold px-5"
+                  />
+                </div>
+              </form>
             </div>
-            <div className="flex justify-evenly gap-5 mt-5">
-            <label htmlFor="">Cantidad:</label>{" "}
-            <input type="number" id="cantidad" name="cantidad" />
-            <input type="submit" value="Agregar" onClick={handleAdd}  className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-out transition-all py-1 rounded-xl bg-orange-500 text-white text-lg fond bold px-5"/>
-            </div>
-          </form>
-          </div>
-          <div className="flex justify-between gap-5 ">
-          <table className="mt-7 w-full">
-            
-            <thead className="bg-amber-500 " >
-              <tr className="">
-                <th className="py-4">Medicamento</th>
-                <th className="py-4">Descripcion</th>
-                <th className="py-4">Cantidad</th>
-                <th className="py-4">Opciones</th>
-              </tr>
-            </thead>
-            <tbody className="">
-              {Medicamentos &&
-                Medicamentos.map((Medicamento, index) => (
-                  <tr key={Medicamento.medicamento}>
-                    <td>{Medicamento.medicamento}</td>
-                    <td>{Medicamento.descripcion}</td>
-                    <td>{Medicamento.cantidad}</td>
-                    <td className="flex justify-between">
-                      <button
-                        className=" bg-orange-400 "
-                        onClick={() => handleDelete(index)}>
-                        <Eliminar />
-                      </button>
-                      <button
-                        className=" bg-orange-400 "
-                        onClick={() => handleEdit(index)}>
-                        <Editar />
-                      </button>
-                    </td>
+            <div className="flex justify-between gap-5 ">
+              <table className="mt-7 w-full">
+                <thead className="bg-amber-500 ">
+                  <tr className="">
+                    <th className="py-4">Medicamento</th>
+                    <th className="py-4">Descripcion</th>
+                    <th className="py-4">Cantidad</th>
+                    <th className="py-4">Opciones</th>
                   </tr>
-                ))}
-            </tbody>
-            
-          </table>
-          </div>
-        </section>
-      </div>
+                </thead>
+                <tbody className="">
+                  {Medicamentos &&
+                    Medicamentos.map((Medicamento, index) => (
+                      <tr key={Medicamento.medicamento}>
+                        <td>{Medicamento.medicamento}</td>
+                        <td>{Medicamento.descripcion}</td>
+                        <td>{Medicamento.cantidad}</td>
+                        <td className="flex justify-between">
+                          <button
+                            className=" bg-orange-400 "
+                            onClick={() => handleDelete(index)}>
+                            <Eliminar />
+                          </button>
+                          <button
+                            className=" bg-orange-400 "
+                            onClick={() => handleEdit(index)}>
+                            <Editar />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </section>
+        </div>
 
-      <button
- className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-out transition-all py-3 rounded-xl bg-orange-500 text-white text-lg fond bold px-5"
-        onClick={() => {
-          toast.promise(loadTasks, {
-            pending: "Guardando la receta",
-            success: "Receta Guardada",
-            error: "Ocurrio un error",
-          });
-        }}>
-        Guardar
-      </button>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-        transition={Slide}
-      />
-
-    </section>
+        <button
+          className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-out transition-all py-3 rounded-xl bg-orange-500 text-white text-lg fond bold px-5"
+          onClick={() => {
+            toast.promise(loadTasks, {
+              pending: "Guardando la receta",
+              success: "Receta Guardada",
+              error: "Ocurrio un error",
+            });
+          }}>
+          Guardar
+        </button>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+          transition={Slide}
+        />
+      </section>
     </div>
   );
 }
